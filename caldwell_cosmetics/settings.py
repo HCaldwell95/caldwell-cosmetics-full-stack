e0cd5659b145e78ea9526edd86686164a0eaaf8c
Hayden Caldwell <hade_caldwell@msn.com>
2024-09-03 10:38:26 +0000

    Debug .env and decouple configuration

--- caldwell_cosmetics/settings.py
+++ caldwell_cosmetics/settings.py
@@ -16,18 +16,15 @@ import os
 import sys
 import dj_database_url
 import environ
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = BASE_DIR / 'templates'
 
-# Initialise environment variables
+# Initialise environment variables using 'environ' library
 env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
-if os.path.exists('env.py'):
-    import env
+# Read environment variables from a .env file
+env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -38,7 +35,6 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
 ALLOWED_HOSTS = [
     'caldwell-cosmetics-full-stack-ecf887f27496.herokuapp.com', 
     'localhost', 
@@ -46,7 +42,6 @@ ALLOWED_HOSTS = [
 ]
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -57,8 +52,6 @@ INSTALLED_APPS = [
     'ckeditor',
     'home_details',
     'treatment_details',
-
-
 ]
 
 MIDDLEWARE = [
@@ -92,6 +85,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'caldwell_cosmetics.wsgi.application'
 
+# Database configuration
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
@@ -103,45 +97,27 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
 # Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+# Heroku settings
+import django_heroku
 django_heroku.settings(locals())
 
 # Default primary key field type
@@ -156,6 +132,7 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
+# Test database configuration
 if 'test' in sys.argv:
     DATABASES = {
         'default': {
